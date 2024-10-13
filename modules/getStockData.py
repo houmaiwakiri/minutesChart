4bbeb678fdacc49e9ca89f8741d50de3b406a248
houmaiwakiri <mahomahoma888@gmail.com>
2024-10-14 00:04:33 +0900

    yahooへ変更

--- modules/getStockData.py
+++ modules/getStockData.py
@@ -1,17 +1,13 @@
-import requests
+import yfinance as yf
 
 def getStockData(symbol):
-    API_KEY = '50YZFP26MACWA5BG'
-    BASE_URL = 'https://www.alphavantage.co/query'
+    # 銘柄コードに「.T」を付ける（日本株の場合）
+    ticker_symbol = symbol + '.T'  # 例: '7203.T'
     
-    parameters = {
-        "function": "TIME_SERIES_DAILY",
-        "symbol": symbol,
-        "apikey": API_KEY,
-        "datatype": "json"
-    }
-    
-    response = requests.get(BASE_URL, params=parameters)
-    data = response.json()
-    
+    # yfinanceを使って株価データを取得
+    stock_data = yf.Ticker(ticker_symbol)
+
+    # 1分足の履歴データを取得（期間を指定して取得）
+    data = stock_data.history(period="1d", interval="1m")  # 1日の1分足データを取得
+
     return data
